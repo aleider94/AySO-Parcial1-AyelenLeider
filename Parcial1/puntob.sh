673db2bcfe9c1057273d2a5c91a9684b40ccc476
aleider94 <ayelenleider0@gmail.com>
2024-10-16 00:28:04 +0000

    actualizamos

--- Parcial1/puntob.sh
+++ Parcial1/puntob.sh
@@ -35,3 +35,21 @@ EOF
 
 echo "particionamiento parcial 1"
 sudo fdisk -l /dev/sdc
+
+echo "formateo de particiones"
+
+sudo mkfs -t ext3 /dev/sdc1 
+sudo mkfs -t ext3 /dev/sdc2 
+sudo mkfs -t ext3 /dev/sdc3
+sudo mkfs -t ext3 /dev/sdc5
+
+sudo lsblk -f
+echo "montamos particiones"
+
+sudo mkdir -p /mnt/disco/{sdc1,sdc2,sdc3,sdc5}
+sudo mount /dev/sdc1/mnt/disco/sdc1
+sudo mount /dev/sdc2/mnt/disco/sdc2
+sudo mount /dev/sdc3/mnt/disco/sdc3
+sudo mount /dev/sdc5/mnt/disco/sdc5
+
+
