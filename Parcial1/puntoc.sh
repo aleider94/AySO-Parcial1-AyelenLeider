b6c4814815b1b1540b33da88379b5c73de669e64
aleider94 <ayelenleider0@gmail.com>
2024-10-18 05:24:42 +0000

    actualizando

--- Parcial1/puntoc.sh
+++ Parcial1/puntoc.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
+creamos los usuarios
+
+sudo useradd -m -s /bin/bash -p "" programadores
+sudo useradd -m -s /bin/bash -p "" tester
+sudo useradd -m -s /bin/bash -p "" tester
+sudo useradd -m -s /bin/bash -p "" analista
+sudo useradd -m -s /bin/bash -p "" disenadores
 
 
+creamos los grupos
+
+sudo groupadd grupoprogramadores
+sudo groupadd grupotester
+sudo groupadd grupoanalistas
+sudo groupadd grupodisenadores
+
+agregamos el usuario al los grupos
+
+sudo usermod -aG grupoprogramadores programadores
+sudo usermod -aG grupotester tester
+sudo usermod -aG grupoanalistas analista
+sudo usermod -aG grupodisenadores disenadores
 
 
+cambiamos el usuario y grupo a las carpetas alumnos
+
+sudo chown programadores:grupoprogramadores alumno_1
+sudo chown tester:grupotester alumno_2
+sudo chown analista:grupoanalistas alumno_3
+sudo chown disenadores:grupodisenadores profesor
 
 
+agregamos persmisos
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- sudo usermod -aG grupoprogramadores programadores
- sudo usermod -aG grupotester tester
- sudo usermod -aG grupoanalistas analistas
- sudo usermod -aG grupodisenadores disenadores
+sudo chmod 750 alumno_1
+sudo chmod 760 alumno_2
+sudo chmod 700 alumno_3
+sudo chmod 775 profesor
 
 
 
---
+++ Parcial1/puntod.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+mkdir -p EstructuraAsimetrica/{correo/{carteros_{1..10},cartas_{1..50}},clientes/cartas_{1..50}}
+
+tree EstructuraAsimetrica
