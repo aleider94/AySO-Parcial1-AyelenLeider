ed88d97134c809f4ce847689ee6964d5c7db1b56
aleider94 <ayelenleider0@gmail.com>
2024-10-18 03:20:03 +0000

    actualizar

---
+++ Parcial1/puntoc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ sudo usermod -aG grupoprogramadores programadores
+ sudo usermod -aG grupotester tester
+ sudo usermod -aG grupoanalistas analistas
+ sudo usermod -aG grupodisenadores disenadores
+
+
+
+
